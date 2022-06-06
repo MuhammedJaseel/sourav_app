2af71ba0bd6f14604fa0cb7795d48e2a91f1e9a1
jaseelPerfectz <jaseelmanmuli@gmail.com>
2022-06-06 11:29:48 +0530

    test

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 app.get("/hallow", (req, res) => {
   res.send("Hello World..........!");
 });
 app.get("/data", (req, res) => {
   res.send({ name: "jaseel", age: 20 }, { name: "sourav", age: 25 });
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
